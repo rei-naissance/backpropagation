2775829f0daac2b10763d6217397c924ce242d88
Philippe Andrei <philippeandreid@gmail.com>
2024-12-06 23:42:00 +0800

    Add files via upload

---
+++ backpropagation/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using System.Xml.Linq;
+
+namespace backpropagation
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.trainNeuralNetButton = new System.Windows.Forms.Button();
+            this.neuralNetOutputBox = new System.Windows.Forms.RichTextBox();
+            this.inputBox1 = new System.Windows.Forms.RichTextBox();
+            this.inputBox2 = new System.Windows.Forms.RichTextBox();
+            this.inputBox3 = new System.Windows.Forms.RichTextBox();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.createNeuralNetButton = new System.Windows.Forms.Button();
+            this.testNeuralNetButton = new System.Windows.Forms.Button();
+            this.inputBox4 = new System.Windows.Forms.RichTextBox();
+            this.testResultBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // trainNeuralNetButton
+            // 
+            this.trainNeuralNetButton.Location = new System.Drawing.Point(224, 174);
+            this.trainNeuralNetButton.Name = "trainNeuralNetButton";
+            this.trainNeuralNetButton.Size = new System.Drawing.Size(123, 52);
+            this.trainNeuralNetButton.TabIndex = 0;
+            this.trainNeuralNetButton.Text = "Train";
+            this.trainNeuralNetButton.UseVisualStyleBackColor = true;
+            // 
+            // neuralNetOutputBox
+            // 
+            this.neuralNetOutputBox.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold);
+            this.neuralNetOutputBox.Location = new System.Drawing.Point(224, 34);
+            this.neuralNetOutputBox.Name = "neuralNetOutputBox";
+            this.neuralNetOutputBox.ReadOnly = true;
+            this.neuralNetOutputBox.Size = new System.Drawing.Size(249, 55);
+            this.neuralNetOutputBox.TabIndex = 1;
+            this.neuralNetOutputBox.Text = "";
+            // 
+            // inputBox1
+            // 
+            this.inputBox1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold);
+            this.inputBox1.Location = new System.Drawing.Point(46, 34);
+            this.inputBox1.Multiline = false;
+            this.inputBox1.Name = "inputBox1";
+            this.inputBox1.Size = new System.Drawing.Size(123, 44);
+            this.inputBox1.TabIndex = 2;
+            this.inputBox1.Text = "";
+            // 
+            // inputBox2
+            // 
+            this.inputBox2.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold);
+            this.inputBox2.Location = new System.Drawing.Point(46, 92);
+            this.inputBox2.Multiline = false;
+            this.inputBox2.Name = "inputBox2";
+            this.inputBox2.Size = new System.Drawing.Size(123, 45);
+            this.inputBox2.TabIndex = 3;
+            this.inputBox2.Text = "";
+            // 
+            // inputBox3
+            // 
+            this.inputBox3.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold);
+            this.inputBox3.Location = new System.Drawing.Point(46, 151);
+            this.inputBox3.Multiline = false;
+            this.inputBox3.Name = "inputBox3";
+            this.inputBox3.Size = new System.Drawing.Size(123, 42);
+            this.inputBox3.TabIndex = 4;
+            this.inputBox3.Text = "";
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(46, 260);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(123, 49);
+            this.resetButton.TabIndex = 5;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            // 
+            // createNeuralNetButton
+            // 
+            this.createNeuralNetButton.Location = new System.Drawing.Point(224, 241);
+            this.createNeuralNetButton.Name = "createNeuralNetButton";
+            this.createNeuralNetButton.Size = new System.Drawing.Size(123, 51);
+            this.createNeuralNetButton.TabIndex = 6;
+            this.createNeuralNetButton.Text = "Create Neural Network";
+            this.createNeuralNetButton.UseVisualStyleBackColor = true;
+            // 
+            // testNeuralNetButton
+            // 
+            this.testNeuralNetButton.Location = new System.Drawing.Point(363, 174);
+            this.testNeuralNetButton.Name = "testNeuralNetButton";
+            this.testNeuralNetButton.Size = new System.Drawing.Size(110, 52);
+            this.testNeuralNetButton.TabIndex = 7;
+            this.testNeuralNetButton.Text = "Test";
+            this.testNeuralNetButton.UseVisualStyleBackColor = true;
+            // 
+            // inputBox4
+            // 
+            this.inputBox4.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold);
+            this.inputBox4.Location = new System.Drawing.Point(46, 209);
+            this.inputBox4.Multiline = false;
+            this.inputBox4.Name = "inputBox4";
+            this.inputBox4.Size = new System.Drawing.Size(123, 40);
+            this.inputBox4.TabIndex = 8;
+            this.inputBox4.Text = "";
+            // 
+            // testResultBox
+            // 
+            this.testResultBox.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.testResultBox.Location = new System.Drawing.Point(224, 101);
+            this.testResultBox.Name = "testResultBox";
+            this.testResultBox.ReadOnly = true;
+            this.testResultBox.Size = new System.Drawing.Size(249, 55);
+            this.testResultBox.TabIndex = 9;
+            this.testResultBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(686, 390);
+            this.Controls.Add(this.testResultBox);
+            this.Controls.Add(this.inputBox4);
+            this.Controls.Add(this.testNeuralNetButton);
+            this.Controls.Add(this.createNeuralNetButton);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.inputBox3);
+            this.Controls.Add(this.inputBox2);
+            this.Controls.Add(this.inputBox1);
+            this.Controls.Add(this.neuralNetOutputBox);
+            this.Controls.Add(this.trainNeuralNetButton);
+            this.Name = "Form1";
+            this.Text = "Neural Network Trainer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button trainNeuralNetButton;
+        private RichTextBox neuralNetOutputBox;
+        private RichTextBox inputBox1;
+        private RichTextBox inputBox2;
+        private RichTextBox inputBox3;
+        private Button resetButton;
+        private Button createNeuralNetButton;
+        private Button testNeuralNetButton;
+        private RichTextBox inputBox4;
+        private RichTextBox testResultBox;
+    }
+}
---
+++ backpropagation/Form1.cs
@@ -0,0 +1,95 @@
+﻿using Backprop;
+using System.Windows.Forms;
+using System;
+
+namespace backpropagation
+{
+    public partial class Form1 : Form
+    {
+        private NeuralNet neuralNet;
+
+        public Form1()
+        {
+            InitializeComponent();
+            AddInputValidation();
+        }
+
+        private void AddInputValidation()
+        {
+            inputBox1.KeyPress += ValidateNumericInput;
+            inputBox2.KeyPress += ValidateNumericInput;
+            inputBox3.KeyPress += ValidateNumericInput;
+            inputBox4.KeyPress += ValidateNumericInput;
+        }
+
+        private void ValidateNumericInput(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && e.KeyChar != '.')
+            {
+                e.Handled = true;
+            }
+
+            if (e.KeyChar == '.' && (sender as RichTextBox).Text.Contains("."))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void TrainNeuralNetButton_Click(object sender, EventArgs e)
+        {
+            if (neuralNet == null)
+            {
+                MessageBox.Show("Please create the neural network first.");
+                return;
+            }
+
+            TrainNeuralNetwork();
+        }
+
+        private void TrainNeuralNetwork()
+        {
+            for (int epoch = 0; epoch < 500; epoch++) {}
+        }
+
+        private void ResetButton_Click(object sender, EventArgs e)
+        {
+            inputBox1.Clear();
+            inputBox2.Clear();
+            inputBox3.Clear();
+            inputBox4.Clear();
+            neuralNetOutputBox.Clear();
+            testResultBox.Clear();
+        }
+
+        private void CreateNeuralNetButton_Click(object sender, EventArgs e)
+        {
+            neuralNet = new NeuralNet(4, 50, 1);
+            testResultBox.Text = "Neural network created.";
+        }
+
+        private void TestNeuralNetButton_Click(object sender, EventArgs e)
+        {
+            if (neuralNet == null)
+            {
+                MessageBox.Show("Please create the neural network first.");
+                return;
+            }
+
+            PerformNeuralNetTest();
+        }
+
+        private void PerformNeuralNetTest()
+        {
+            neuralNet.setInputs(0, Convert.ToDouble(inputBox1.Text));
+            neuralNet.setInputs(1, Convert.ToDouble(inputBox2.Text));
+            neuralNet.setInputs(2, Convert.ToDouble(inputBox3.Text));
+            neuralNet.setInputs(3, Convert.ToDouble(inputBox4.Text));
+
+            neuralNet.run();
+
+            double output = neuralNet.getOuputData(0);
+            neuralNetOutputBox.Text = output.ToString();
+            testResultBox.Text = output > 0.6 ? "True" : "False";
+        }
+    }
+}
